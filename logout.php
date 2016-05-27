98501a141aa9c10d2cf02fe93755cb6807b6d02c
davisnju <david1203@163.com>
2016-05-27 08:57:53 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-$_SESSION['isLogged']=0;	
-//echo "session_logged:".$_SESSION['isLogged'];
-$url = "davisBlog_mg1515007.php";  
-echo "<script language='javascript' type='text/javascript'>";  
-echo "window.location.href='$url'";  
-echo "</script>";
-?>
